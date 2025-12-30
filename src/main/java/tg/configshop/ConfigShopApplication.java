d9551228a2e845be7a097294a7cea0467c3345f0
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-12-30 21:00:23 +0400

    first commit

---
+++ src/main/java/tg/configshop/ConfigShopApplication.java
@@ -0,0 +1,13 @@
+package tg.configshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tg/configshop/ConfigShopApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.configshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
