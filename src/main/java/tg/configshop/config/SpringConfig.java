0498f33968e8d9db3019a276dfb59eb056efe81d
ZhenShen.ITIS <ewgenii20066@gmail.com>
2026-01-17 23:42:15 +0300

    fix11

--- src/main/java/tg/configshop/config/SpringConfig.java
+++ src/main/java/tg/configshop/config/SpringConfig.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -52,6 +53,8 @@ public class SpringConfig {
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        mapper.registerModule(new ParameterNamesModule());
 
 
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
